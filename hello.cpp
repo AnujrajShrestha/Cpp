fc793f95b4107bd8126895a29982239735c08ee3
AnujrajShrestha <shrestharachana196@gmail.com>
2026-07-30 18:36:10 +0545

    new

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    //this is a command
+    /*This is a 
+    multi-line command*/
+    std::cout << "hello" << std::endl;
+    std::cout <<"how are you" << std::endl;
+    return 0;
+}
