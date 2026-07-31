0f7f1396d305368676ac96bf77b83f04e9461a29
AnujrajShrestha <shrestharachana196@gmail.com>
2026-07-31 13:55:39 +0545

    updated

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // int main(){
@@ -30,8 +31,57 @@ using namespace std;
 //     return 0;
 // }
 
+// int main(){
+//     int x= 5;
+//     bool result= (x>10) && (++x>0);
+//     cout << result;
+// }
+
+// int main(){
+//     int a;
+//     cout << "Enter a number: ";
+//     cin >> a;
+//     cout << a << '\n' ;
+// }
+
+// int main(){
+//     int a,b;
+//     cout <<"Enter the frist number: ";
+//     cin >> a;
+//     cout <<"Enter the second number: ";
+//     cin >> b;
+//     int sum = a+b;
+//     cout << "The sum of "<< a << " and " << b << " is " << ": " << sum << '\n';
+//    return 0;
+// }
+
+// int main(){
+//    int age;
+//    string name;
+//    cout << "Enter your name: ";
+//    getline(cin, name);
+// //    cin >> name;
+//    cout << "Enter your age: ";
+//    cin >> age;
+//    cout << "Hello " << name << ", you are " << age << " years old.";
+//    return 0;
+// }
+
+// int main(){
+//     int a,b,c;
+//     double avg=0;
+//     cout << "Enter any three numbers: ";
+//     cin >> a >> b >> c;
+//     avg=(double)(a+b+c)/3;
+//     cout << "The aveage of " << a << "," << b << " and " << c << " is: " << avg <<'\n';
+//     return 0; 
+// }
+
 int main(){
-    int x= 5;
-    bool result= (x>10) && (++x>0);
-    cout << result;
+    int a,b;
+    cout << "Enter any two numbers: ";
+    cin >> a >> b;
+    a=a^b^(b=a);
+    cout << "a = " << a << ",b = " << b << '\n';
+    return 0;
 }
