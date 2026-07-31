b77f2ddcfd75947bbad40a3bc621f9c6a6dfa871
AnujrajShrestha <shrestharachana196@gmail.com>
2026-07-31 10:39:09 +0545

    updated

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//     //this is a command
+//     /*This is a 
+//     multi-line command*/
+//     cout << "hello" << std::endl;
+//     cout <<"how are you" << '\n';
+//     int num;//variable declaration
+//     num= 10; //variable initialization
+//     cout << "num: " << num << '\n';
+//     int num2= 10;
+//     int sum= num+num2;
+//     cout << "Sum: " << sum << '\n';
+//     cout << sizeof(sum) << '\n';
+//     return 0;
+// }
+
+// int main(){
+// //     double pi= 3.9;
+// //     int x= (int)pi;
+// //     cout << x;
+//     // char ch= 'a';
+//     // cout << ch+1 <<'\n';
+//     int a=10,b=10;
+//     cout << ++a <<'\n';
+//     cout << b++ <<'\n';
+//     cout << b << '\n';
+//     return 0;
+// }
+
 int main(){
-    //this is a command
-    /*This is a 
-    multi-line command*/
-    cout << "hello" << std::endl;
-    cout <<"how are you" << '\n';
-    int num;//variable declaration
-    num= 10; //variable initialization
-    cout << "num: " << num << '\n';
-    int num2= 10;
-    int sum= num+num2;
-    cout << "Sum: " << sum << '\n';
-    return 0;
+    int x= 5;
+    bool result= (x>10) && (++x>0);
+    cout << result;
 }
