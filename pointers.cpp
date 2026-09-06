22857fae92e7786ae87c60e1147c5c233e767e2f
AnujrajShrestha <shrestharachana196@gmail.com>
2026-09-06 12:21:11 +0545

    pointers

--- pointers.cpp
+++ pointers.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main(){
     int x= 10;
     int *ptr= &x;
+    *ptr= *ptr * 5;
+    (*ptr)++;
     cout << &x << '\n';  //address
     cout << ptr << '\n'; //address
-    cout << *ptr << '\n'; //10
+    cout << *ptr << '\n'; //51 
     cout << &ptr << '\n'; //address of address
     return 0;
 }
