9474f0776c245cff8f80a578fc20473b37604583
AnujrajShrestha <shrestharachana196@gmail.com>
2026-09-06 12:31:23 +0545

    pointers

--- pointers.cpp
+++ pointers.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//     int x= 10;
+//     int *ptr= &x;
+//     *ptr= *ptr * 5; // Arithmetic opreations
+//     (*ptr)++;
+//     cout << &x << '\n';  //address
+//     cout << ptr << '\n'; //address
+//     cout << *ptr << '\n'; //51 
+//     cout << &ptr << '\n'; //address of address
+//     return 0;
+// }
+
+//comparsion using pointers
 int main(){
     int x= 10;
+    int y= 10;
     int *ptr= &x;
-    *ptr= *ptr * 5;
-    (*ptr)++;
-    cout << &x << '\n';  //address
-    cout << ptr << '\n'; //address
-    cout << *ptr << '\n'; //51 
-    cout << &ptr << '\n'; //address of address
+    int *ptr2= &y;
+    cout << (*ptr == *ptr2) << '\n'; //-> 1
+    cout << (*ptr > *ptr2) << '\n'; // -> 0
     return 0;
 }
